1784c0327647f124264179d103339fcf4b9506bf
ECastanon <ecastanon157@gmail.com>
2025-08-15 22:26:04 -0700

    Initial commit

---
+++ Assets/BuildTile.cs
@@ -0,0 +1,21 @@
+using System.Net;
+using UnityEngine;
+
+public class BuildTile : MonoBehaviour
+{
+    public GameObject towerOnTile;
+
+    public void SetTowerOnTile(GameObject towerToPlace)
+    {
+        //Can Add Other Rescoure Checks Here
+        if (towerOnTile == null)
+        {
+            GameObject tower = Instantiate(towerToPlace, transform.position, Quaternion.identity);
+            towerOnTile = tower;
+        }
+        else
+        {
+            Debug.LogWarning("Cannot Add Another Tower!");
+        }
+    }
+}
---
+++ Assets/SimpleMovement.cs
@@ -0,0 +1,68 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+
+public class SimpleMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody rb;
+    public LayerMask btLayer;
+    public bool isHittingBuildTile;
+    public BuildTile bt;
+
+    public List<GameObject> Towers = new List<GameObject>();
+
+    public Transform endPoint;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        PerformRaycast();
+        BuildTower();
+        Move();
+    }
+    void Move()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+        movement = transform.TransformDirection(movement) * speed * Time.deltaTime;
+        rb.MovePosition(rb.position + movement);
+    }
+
+    void BuildTower()
+    {
+        if (isHittingBuildTile && Input.GetKeyDown(KeyCode.Space))
+        {
+            if (bt != null)
+            {
+                bt.SetTowerOnTile(Towers[0]);
+            }
+        }
+    }
+
+    public void PerformRaycast()
+    {
+        RaycastHit hit;
+        Vector3 direction = endPoint.position - transform.position;
+        if (Physics.Raycast(transform.position, direction.normalized, out hit, direction.magnitude, btLayer))
+        {
+            Debug.Log("Hit: " + hit.collider.name);
+            isHittingBuildTile = true;
+            bt = hit.collider.GetComponent<BuildTile>();
+        }
+        else
+        {
+            isHittingBuildTile = false;
+            bt = null;
+        }
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.color = isHittingBuildTile ? Color.green : Color.red;
+        Gizmos.DrawLine(transform.position, endPoint.position);
+    }
+}
